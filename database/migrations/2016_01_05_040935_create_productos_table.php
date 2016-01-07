bf28fbdc53fbd21e5ad90a974d3d1bb9087aa0f2
David Pascual <dpmtz07@gmail.com>
2016-01-07 17:37:13 -0600

    Migrates MegaMaderas

---
+++ database/migrations/2016_01_05_040935_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('productos', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('nombre');
+			$table->text('descripcion');
+			$table->float('precio');
+			$table->integer('tipo_id')->unsigned();
+			$table->foreign('tipo_id')->references('id')->on('tipos');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('productos');
+	}
+
+}
