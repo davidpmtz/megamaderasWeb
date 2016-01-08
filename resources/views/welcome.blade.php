1146ddd60eb31e5c6fc7ff6d08dab8e609ee486b
Waskalle <miki_9_7@hotmail.com>
2016-01-07 21:26:59 -0600

    Nuevo template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,222 +1,252 @@
+<!-- =============================================== -->
+<!-- =                                             = -->
+<!-- =                JMED 						             = -->
+<!-- =             Enterprise                      = -->
+<!-- =          										               = -->
+<!-- =============================================== -->
+
+
 <!DOCTYPE html>
-<html lang="en">
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
 <head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>Mega Maderas</title>
+	<meta name="description" content="">
+	<meta name="author" content=" Made by Keyners">
+    <meta http-equiv="X-UA-Compatible" content="IE=9" />
 
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!-- PT Sans -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+	<!-- Crete Roung -->
+	<link href='http://fonts.googleapis.com/css?family=Crete+Round&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+	<!-- CSS
+  ================================================== -->
+  	<link rel="stylesheet" href="{{asset('css/reset.css')}}">
+	<link rel="stylesheet" href="{{asset('css/base.css')}}">
+	<link rel="stylesheet" href="{{asset('css/skeleton.css')}}">
+	<link rel="stylesheet" href="{{asset('css/layout.css')}}">
+
 	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-	<!-- Meta Tags -->
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
-	<title>MegaMaderas</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js"></script>
+	<script type="text/javascript" src="{{asset('js/validate.js')}}"></script>
+	<script type="text/javascript" src="{{asset('js/fancybox/jquery.fancybox-1.3.4.pack.js')}}"></script>
+	<link rel="stylesheet" type="text/css" href="{{asset('js/fancybox/jquery.fancybox-1.3.4.css')}}" media="screen" />
+	<script type="text/javascript">
+		$(document).ready(function() {
+				$("a[rel=example_group]").fancybox({
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none',
+				'titlePosition' 	: 'over',
+				'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+					return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+				}
+			});
+		});
+	</script>
 
-	<meta name="description" content="Insert Your Site Description" />
-
-	<!-- Mobile Specifics -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="HandheldFriendly" content="true"/>
-	<meta name="MobileOptimized" content="320"/>
-
-	<!-- Mobile Internet Explorer ClearType Technology -->
-	<!--[if IEMobile]>  <meta http-equiv="cleartype" content="on">  <![endif]-->
-
-	<!-- Bootstrap -->
-	<link href="{{asset('_include/css/bootstrap.min.css')}}" rel="stylesheet">
-
-	<!-- Main Style -->
-	<link href="{{asset('_include/css/main.css')}}" rel="stylesheet">
-	<link href="{{asset('css/principal.css')}}" rel="stylesheet">
-
-	<!-- Supersized -->
-	<link href="{{asset('_include/css/supersized.css')}}" rel="stylesheet">
-	<link href="{{asset('_include/css/supersized.shutter.css')}}" rel="stylesheet">
-
-	<!-- FancyBox -->
-	<link href="{{asset('_include/css/fancybox/jquery.fancybox.css')}}" rel="stylesheet">
-
-	<!-- Font Icons -->
-	<link href="{{asset('_include/css/fonts.css')}}" rel="stylesheet">
-
-	<!-- Shortcodes -->
-	<link href="{{asset('_include/css/shortcodes.css')}}" rel="stylesheet">
-
-	<!-- Responsive -->
-	<link href="{{asset('_include/css/bootstrap-responsive.min.css')}}" rel="stylesheet">
-	<link href="{{asset('_include/css/responsive.css')}}" rel="stylesheet">
-
-	<!-- Supersized -->
-	<link href="{{asset('_include/css/supersized.css')}}" rel="stylesheet">
-	<link href="{{asset('_include/css/supersized.shutter.css')}}" rel="stylesheet">
-
-	<!-- Google Font -->
-	<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900' rel='stylesheet' type='text/css'>
-
-	<!-- Fav Icon -->
-	<link rel="shortcut icon" href="#">
-
-	<link rel="apple-touch-icon" href="#">
-	<link rel="apple-touch-icon" sizes="114x114" href="#">
-	<link rel="apple-touch-icon" sizes="72x72" href="#">
-	<link rel="apple-touch-icon" sizes="144x144" href="#">
 </head>
 <body>
-	<div class="ole">
-	<section id="jSplash">
-		<div id="circle"></div>
-	</section>
-	</div>
-	<!-- End of Splash Screen -->
-	<!-- Header -->
+
+
 	<header>
-	    <div class="sticky-nav">
-	    	<a id="mobile-nav" class="menu-nav" href="#menu-nav"></a>
-
-	        <div id="logo">
-	        	<a id="goUp" href="#home-slider" title="Brushed | Responsive One Page Template">Brushed Template</a>
-	        </div>
-
-	        <nav id="menu">
-	        	<ul id="menu-nav">
-	            	<li class="current"><a id="inicio">Inicio</a></li>
-	                <li><a id="productos">Nuestros Productos</a></li>
-	                <li><a id="nosotros">Acerca de nosotros</a></li>
-	                <li><a id="contacto">Contactanos</a></li>
-	            </ul>
-	        </nav>
+		<nav>
+			<div class='container'>
+				<div class='five columns logo'>
+					<a href='#'>Company Logo</a>
 				</div>
-		</header>
-<!-- End Header -->
-					<!-- Homepage Slider -->
-					<div id="home-slider">
-					  <div class="overlay"></div>
-					  <div class="slider-text">
-					  </div>
-						<div class="control-nav">
-					    <a id="prevslide" class="load-item"><i class="font-icon-arrow-simple-left"></i></a>
-					    <a id="nextslide" class="load-item"><i class="font-icon-arrow-simple-right"></i></a>
-					    <ul id="slide-list"></ul>
-					        <a id="nextsection" href="#work"><i class="font-icon-arrow-simple-down"></i></a>
-					    </div>
-					</div>
-					<!-- End Homepage Slider -->
 
-					<div id="work" class="page" style="display:none">
+				<div class='eleven columns'>
+					<ul class='mainMenu'>
+						<li><a href='index.html' title='Home'>Home</a></li>
+						<li><a href='#' title='About us'>About us</a></li>
+						<li><a href='#' title='Pricing'>Pricing</a></li>
+						<li><a href='#' title='Blog'>Blog</a></li>
+						<li><a href='#' title='Portfolio'>Portfolio</a></li>
+						<li><a href='contact' title='Contact'>Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 
-					  <!-- Our Work Section -->
-					  	<div class="container">
-					      	<!-- Title Page -->
-					          <div class="row">
-					              <div class="span12">
-					                  <div class="title-page">
-					                      <h2 class="title">Nuestros Productos</h2>
-					                  </div>
-					              </div>
-					          </div>
-					          <!-- End Title Page -->
-					          <!-- Portfolio Projects -->
-					          <div class="row">
-					          	<div class="span3">
-					              	<!-- Filter -->
-					                  <nav id="options" class="work-nav">
-					                      <ul id="filters" class="option-set" data-option-key="filter">
-					                        <li class="type-work">CATEGORIAS DE PRODUCTO</li>
-					                          <li><a href="#filter" data-option-value="*" class="selected">Todos Los Productos</a></li>
-					                          <li><a href="#filter" data-option-value=".video">Cimbra</a></li>
-					                          <li><a href="#filter" data-option-value=".photography">Herrajes</a></li>
-					                          <li><a href="#filter" data-option-value=".design">Maderas</a></li>
-					                      </ul>
-					                  </nav>
-					                  <!-- End Filter -->
-					              </div>
-					              <div class="span9">
-					              	<div id="productosm" class="row">
-					              	</div>
-					              </div>
-					          </div>
-					          <!-- End Portfolio Projects -->
-					      </div>
-					  <!-- End Our Work Section -->
+		<div class='container'>
+			<div class='slogan'>
+				<div class='ten columns'>
+					<h1>Big catchy title</h1>
+					<h2>Bit smaller but still in need slogan</h2>
+				</div>
 
-					</div>
-					<div id="about" class="page-alternate" style="display:none">
-					</div>
-					<div id="contact" class="page" style="display:none">
-					</div>
-					<!-- Socialize -->
-					<div id="social-area" class="page" style="padding:50px;">
-						<div class="container">
-					    	<div class="row">
-					            <div class="span12">
-					                <nav id="social">
-					                    <ul>
-					                        <li><a href="https://twitter.com/" title="Follow Me on Twitter" target="_blank"><span class="font-icon-social-twitter"></span></a></li>
-					                        <li><a href="https://www.facebook.com/" title="Follow Me on Facebook" target="_blank"><span class="font-icon-social-facebook"></span></a></li>
-					                        <li><a href="https://plus.google.com/" title="Follow Me on Google Plus" target="_blank"><span class="font-icon-social-google-plus"></span></a></li>
-					                        <li><a href="http://www.linkedin.com/in/" title="Follow Me on LinkedIn" target="_blank"><span class="font-icon-social-linkedin"></span></a></li>
-					                    </ul>
-					                </nav>
-					            </div>
-					        </div>
-					    </div>
-					</div>
-					<!-- End Socialize -->
+				<div class='six columns'>
+					<h4>Offer EXCLUSIVE</h4>
+					<p>Nunc rhoncus, erat quis sagittis convallis, arcu tellus tempor felis, sed tempor ipsum lorem vitae dolor. Morbi blandit condimentum lectus, id porta est sagittis et. Integer eget tortor sit amet ante eleifend lacinia quis ut</p>
+					<a href='#' class='button medium green'>See the price</a>
+				</div>
+			</div>
+		</div>
+	</header>
 
-					<!-- Footer -->
-					<footer>
-						<p class="credits">&copy;2015 Mega-Maderas. <a class="credits2" title="Brushed | Responsive One Page Template">Es un desarrolo </a> de <a href="http://www.alessioatzeni.com/" title="Alessio Atzeni | Web Designer &amp; Front-end Developer">J-MED Enterprise</a></p>
-					</footer>
-					<!-- End Footer -->
 
-					<!-- Back To Top -->
-					<a id="back-to-top" href="#">
-						<i class="font-icon-arrow-simple-up"></i>
-					</a>
-@yield('content')
+	<div class='clear'></div>
+	<div class='clear'></div>
 
-@yield('js')
-	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+	<div class='container'>
+
+		<div class='one-third column'>
+			<img src="{{asset('images/misc/about_us.png')}}">
+			<h3>About Us</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+		</div>
+
+
+		<div class='one-third column'>
+			<img src="{{asset('images/misc/team.png')}}">
+			<h3>The Team</h3>
+			<p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur.</p>
+		</div>
+
+
+
+		<div class='one-third column'>
+			<img src="{{asset('images/misc/goals.png')}}">
+			<h3>Goals</h3>
+			<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+
+	</div>
+
+
+	<div class='clear'></div>
+
+	<div class='container focus'>
+
+		<h3>Packages</h3>
+
+		<div class='eight columns'>
+			<h4>WRITING</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+		</div>
+
+		<div class='eight columns'>
+			<h4>PRESENTATION</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+		</div>
+
+
+		<div class='clear'></div>
+
+
+		<div class='eight columns'>
+			<h4>NEGOTIATING</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+		</div>
+
+		<div class='eight columns'>
+			<h4>GRAMMAR</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+		</div>
+
+	</div>
+
+
+
+
+	<footer>
+		<div class='container'>
+
+			<div class='eight columns'>
+				<h5>Some stuff can go here</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+			</div>
+
+			<div class='four columns social'>
+				<h5>Social media</h5>
+				<a href='#'><img src="images/social/dribbble.png"></a>
+				<a href='#'><img src="{{asset('images/social/facebook.png')}}"></a>
+				<a href='#'><img src="{{asset('images/social/twitter.png')}}"></a>
+				<a href='#'><img src="{{asset('images/social/forrst.png')}}"></a>
+			</div>
+
+			<div class='four columns'>
+				<h5>Get in touch</h5>
+				<p>You can call us at:<br>
+					t: 161 - 123 - CALL - NOW</p>
+				<p>Or drop us a line on:
+					e: <a href='mailto:contact@free.com'>contact@free.com</a></p>
+			</div>
+
+
+		<a id='top' href='#'>&uarr;</a>
+		</div>
+	</footer>
 	<script type="text/javascript">
-  var _smartsupp = _smartsupp || {};
-  _smartsupp.key = 'f71cd350e0a2909f8bbb417ef623274ef167b349';
-  window.smartsupp||(function(d) {
-  	var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  	s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  	c.type='text/javascript';c.charset='utf-8';c.async=true;
-  	c.src='//www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-  })(document);
-  </script>
-<script> src='http://maps.googleapis.com/maps/api/js'></script>
-<script src="_include/js/modernizr.js"></script>
+		var form = $('form');
+
+		$(document).ready(function(){
+		    form.validate({
+		    	ignore: "",
+	            rules: {
+	            	'message': {
+	                    required: true,
+	                },
+	            	'name': {
+	                    required: true,
+	                },
+	                'mail': {
+	                    required: true,
+	                    email: true
+	                }
+	            },
+	            errorPlacement: function(error, element){}
+
+		    });
+		  });
+	</script>
 
 
-<!-- Analytics -->
-<script type="text/javascript">
+	<script type="text/javascript">
+		var toper = $('a#top');
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'Insert Your Code']);
-  _gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+		$(window).scroll(function(){
+            if ($(this).scrollTop() > 100) {
+                toper.fadeIn( 200 );
+            } else {
+                toper.fadeOut( 200 );
+            }
+        });
 
-</script>
+         toper.click(function(){
+        	$('html, body').animate({scrollTop:0}, 500);
+        	return false;
+    	});
+	</script>
+
 
 </body>
 </html>
